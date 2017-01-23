17a09a4a829765c9eb26f14e13b91ae7a63c5302
Domenico Barra <eisenach@gmail.com>
2017-01-23 19:43:28 +0100

    Added simplelink_wrapper.hpp

--- src/arm/ti-simplelink/src/mqtt_transport_impl.cpp
+++ src/arm/ti-simplelink/src/mqtt_transport_impl.cpp
@@ -43,20 +43,7 @@
  ****************************************************************************/
 
 
-/*
-   CAUTION!!!
-     A bug in TI's simplelink.h header (at least in version 1.0.1.6) causes an
-     extern "C" declaration to be opened once and closed twice within that
-     file. Here we work around this problem by using nesting, so we open an
-     extern "C" before including "simplelink.h", and then test the specific
-     SimpleLink version to check if we need to close it.
-   TODO: keep track of future SimpleLink versions
- */
-extern "C" {
-#include "simplelink.h"
-#if (! (SL_MAJOR_VERSION_NUM==1L && SL_MINOR_VERSION_NUM==0L && SL_VERSION_NUM==1L && SL_SUB_VERSION_NUM==6L ))
-    } // close extern "C"
-#endif
+#include "simplelink_wrapper.hpp"
 
 #include "gv/mqtt_transport.hpp"
 #include "mqtt_transport_impl.hpp"
